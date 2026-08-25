a43b604bc882b723bdf94c56f6d26b5a8f755d7c
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-25 20:47:14 +0100

    mui: sound label + button

--- mui.h
+++ mui.h
@@ -132,10 +132,20 @@ static void uv(float * uv, char c) {
   uv[0] = uv[1] = uv[2] = uv[3] = 0;
 }
 
-static int mui_draw_icon(const mui_api_t * t, float rect[4], float dim, float id) {
-  int hover =
+static int mui_strlen(const char * str) {
+  int len = 0;
+  for (const char * c = str; *c; c++) len += mui_font_width(*c) * 3;
+  return len;
+}
+
+static int mui_hover(float rect[4]) {
+  return
     mui_mx >= rect[0] && mui_mx < (rect[0] + rect[2]) &&
     mui_my >= rect[1] && mui_my < (rect[1] + rect[3]);
+}
+
+static int mui_draw_icon(const mui_api_t * t, float rect[4], float dim, float id) {
+  int hover = mui_hover(rect);
   if (hover) {
     dim = 1;
 
@@ -151,29 +161,22 @@ static int mui_draw_icon(const mui_api_t * t, float rect[4], float dim, float id
 
   return hover && mui_mu && (mui_mid == id);
 }
-static int mui_draw_window(const mui_api_t * t) {
-  const int ww = 340;
-  const int wh = 200;
-
-  int wx = (t->sw - ww) / 2;
-  int wy = (t->sh - wh) / 2;
-  int hover_out =
-    mui_mx < wx || mui_mx > wx + ww ||
-    mui_my < wy || mui_my > wy + wh;
+static int mui_draw_window(const mui_api_t * t, float rect[4]) {
+  int hover_out = !mui_hover(rect);
   if (hover_out && mui_md) mui_mid = 1;
   if (hover_out && mui_mu && mui_mid == 1) {
     return 0;
   }
 
   mui_upc_t pc = {
-    .rect   = { wx - 2, wy - 2, ww + 4, wh + 4 },
+    .rect   = { rect[0] - 2, rect[1] - 2, rect[2] + 4, rect[3] + 4 },
     .colour = { 0.27, 0.47, 0.35, 0xFFFF },
     .extent = { t->sw, t->sh },
   };
   t->draw(t->ptr, &pc);
 
   pc = (mui_upc_t) {
-    .rect   = { wx, wy, ww, wh },
+    .rect   = { rect[0], rect[1], rect[2], rect[3] },
     .colour = { 0.04, 0.12, 0.08, 0xFFFF },
     .extent = { t->sw, t->sh },
   };
@@ -181,21 +184,73 @@ static int mui_draw_window(const mui_api_t * t) {
   return 1;
 }
 
+static void mui_draw_str(const mui_api_t * t, const char * str, float x, float y) {
+  mui_upc_t pc = {
+    .rect   = { x, y, 0, 15 },
+    .colour = { 0.9, 0.9, 0.9, 0 },
+    .extent = { t->sw, t->sh },
+  };
+  for (const char * c = str; *c; c++) {
+    pc.rect[2] = mui_font_width(*c) * 3;
+    uv(pc.uv, *c);
+    t->draw(t->ptr, &pc);
+    pc.rect[0] += pc.rect[2] + 2;
+  }
+}
+
+static int mui_draw_btn(const mui_api_t * t, int id, const char * str, float rect[4]) {
+  float dim = 0.8;
+  int hover = mui_hover(rect);
+  if (hover) {
+    dim = 1;
+    if (mui_md) mui_mid = id;
+  }
+
+  mui_upc_t pc = {
+    .rect   = { rect[0] - 2, rect[1] - 2, rect[2] + 4, rect[3] + 4 },
+    .colour = { 0.04, 0.12, 0.08, 0xFFFF },
+    .extent = { t->sw, t->sh },
+  };
+  t->draw(t->ptr, &pc);
+
+  pc = (mui_upc_t) {
+    .rect   = { rect[0], rect[1], rect[2], rect[3] },
+    .colour = { dim * 0.27, dim * 0.47, dim * 0.35, 0xFFFF },
+    .extent = { t->sw, t->sh },
+  };
+  t->draw(t->ptr, &pc);
+
+  float w = mui_strlen(str);
+  mui_draw_str(t, str, rect[0] + (rect[2] - w) / 2, rect[1] + (rect[3] - 15) / 2);
+  return hover && mui_mu && (mui_mid == id);
+}
+
 static float mui_lvl = 1;
 
-static int mui_st_options = 0;
+static int mui_st_options = 1;
 static void mui_guarded_run(const mui_api_t * t) {
   if (mui_draw_icon(t, (float[4]) { t->sw - 48 - 12, 12, 48, 48 }, 0.8, 0xEE00)) {
     mui_st_options = !mui_st_options;
   }
   if (!mui_st_options) return;
-  if (!mui_draw_window(t)) {
+
+  int wx = (t->sw - 340) / 2;
+  int wy = (t->sh - 200) / 2;
+  if (!mui_draw_window(t, (float[4]) { wx, wy, 340, 200 })) {
     mui_st_options = 0;
     return;
   }
 
-  int wx = (t->sw - 300) / 2;
-  int wy = (t->sh - 200) / 2;
+  float cl = wx + 16;
+  float cr = wx + 340 - 16;
+  float r1 = wy + 32;
+
+  mui_draw_str(t, "Sound", cl, r1);
+
+  const char * sfx = sfx_enabled() ? "ON" : "OFF";
+  if (mui_draw_btn(t, 2, sfx, (float[4]) { cr - 60, r1 - 8, 60, 15 + 16 })) {
+    sfx_toggle();
+  }
 
   mu_begin(&mui_ctx);
 
@@ -209,8 +264,8 @@ static void mui_guarded_run(const mui_api_t * t) {
     mui_vspace(6);
 
     mu_layout_row(&mui_ctx, 3, (int[]) { -60, -1 }, 32);
-    mui_label("Sound");
-    if (mu_button(&mui_ctx, sfx_enabled() ? "ON" : "")) sfx_toggle();
+    mui_label("");
+    mui_label("");
 
     mui_vspace(12);
 
