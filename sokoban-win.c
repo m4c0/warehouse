3ca8b6099440d9cae6def6d0ada42f845288433e
Eduardo Costa <m4c0@users.noreply.github.com>
2026-08-29 13:03:38 +0100

    win: fix alpha blend of mui

--- sokoban-win.c
+++ sokoban-win.c
@@ -348,6 +348,14 @@ static int d3d_init_pso_mui() {
     },
   };
   desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+  desc.BlendState.RenderTarget[0].BlendEnable           = TRUE;
+  desc.BlendState.RenderTarget[0].SrcBlend              = D3D12_BLEND_SRC_ALPHA;
+  desc.BlendState.RenderTarget[0].DestBlend             = D3D12_BLEND_INV_SRC_ALPHA;
+  desc.BlendState.RenderTarget[0].BlendOp               = D3D12_BLEND_OP_ADD;
+  desc.BlendState.RenderTarget[0].SrcBlendAlpha         = D3D12_BLEND_ONE;
+  desc.BlendState.RenderTarget[0].DestBlendAlpha        = D3D12_BLEND_INV_SRC_ALPHA;
+  desc.BlendState.RenderTarget[0].BlendOpAlpha          = D3D12_BLEND_OP_ADD;
+  desc.BlendState.RenderTarget[0].LogicOp               = D3D12_LOGIC_OP_NOOP;
   desc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
   D3D_CHK(d3d_device, CreateGraphicsPipelineState, &desc, &IID_ID3D12PipelineState, (void **)&d3d_pso_mui);
 
